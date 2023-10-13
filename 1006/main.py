34444b1b7fcd653f6cd53498a00ec64bfc648e3f
Hsun <hsun1031@outlook.com>
2023-10-13 19:57:42 +0800

    Update main.py

--- 1006/main.py
+++ 1006/main.py
@@ -2,19 +2,24 @@ from numpy import random
 
 my_list = random.randint(0, 2, size=8 * 3)
 
-for i in range(0, len(my_list), 8):
-    row = my_list[i:i + 8]
-    print(row)
-
 true_list = []
 out_list = []
 
-for i in range(0, 3, 1):
+def to_hex(list):
     num = 0
-    for j in range(0, 8, 1):
-        num = num + my_list[j + i * 8] * (2 ** (7 - j))
+    for i in range(0, 8, 1):
+        num = num + list[i] * (2 ** (7 - i))
+    return num
+
+for i in range(0, len(my_list), 8):
+    row = my_list[i:i + 8]
+    print(row)
+    num = to_hex(row)
     true_list.append(num)
 
+
+
+for num in true_list:
     # -100~100
     num = num - 128
     if num > 100:
